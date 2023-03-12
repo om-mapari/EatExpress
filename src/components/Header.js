72e501e66fe81cb3dd8969e07999ac551920edbc
om mapari <mapariom05@gmail.com>
2023-03-13 03:20:46 +0530

    login button added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,25 @@
+import { useState } from "react";
 import logo from "../logo.png";
 
 function Header() {
+    const [loggedIn, setLoggedIn] = useState(false);
     return (
-        
-            <div className="header">
-                <img className="logo" src={logo} alt="logo" />
+        <div className="header">
+            <img className="logo" src={logo} alt="logo" />
 
-                <div className="nav-items">
-                    <ul>
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Contact</li>
-                        <li>Cart</li>
-                    </ul>
-                </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
             </div>
 
+            <button onClick={() => setLoggedIn(!loggedIn)}>
+                {loggedIn ? "logOut" : "logIn"}
+            </button>
+        </div>
     );
 }
 
