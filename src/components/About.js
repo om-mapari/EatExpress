b20c9aecbb4f8c9d108263a2e7437607abc818e2
om mapari <mapariom05@gmail.com>
2023-03-14 02:15:05 +0530

    About file added

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        <h1>About us Page</h1> 
+        <p>This is a about us page</p>
+    </div>
+  )
+}
+
+export default About
