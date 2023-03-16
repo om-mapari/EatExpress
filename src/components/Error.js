7becafab1dcd1f49c940dfaff410cff54e237df4
om mapari <mapariom05@gmail.com>
2023-03-16 19:04:45 +0530

    error page for not existing route

--- src/components/Error.js
+++ src/components/Error.js
@@ -5,10 +5,12 @@ import { useRouteError } from "react-router-dom";
 function Error() {
     const err = useRouteError();
     console.log(err);
+    const { status, statusText } = err;
     return (
         <div>
             <h1>Oops!!</h1>
             <h2>something went's wrong</h2>
+            <h2>{status + " : " + statusText}</h2>
         </div>
     );
 }
